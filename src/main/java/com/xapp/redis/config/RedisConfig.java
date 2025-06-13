968e7745d7164ff6a77f801b170518491e562f48
Ajay Kumar Manjhi <ajay.cse.nitjsr@gmail.com>
2025-06-13 13:38:17 +0530

    Initial Commit - Product Crud REST endpoints with GraphQL

--- src/main/java/com/xapp/redis/config/RedisConfig.java
+++ src/main/java/com/xapp/redis/config/RedisConfig.java
@@ -11,6 +11,7 @@ public class RedisConfig {
 
   @Bean
   public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+
     RedisTemplate redisTemplate = new RedisTemplate();
     redisTemplate.setConnectionFactory(factory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
